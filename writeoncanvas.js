d6cd04db2062012c51838faf1df64709e9a78bf9
haangeline <angelineha74@gmail.com>
2022-02-08 22:34:17 +0100

    header nav bar & sidebar
    finalisation de la barre de navigation en tête du site
    ajout d'une sidebar permettant d'accéder aux différents TP (à améliorer)

---
+++ writeoncanvas.js
@@ -0,0 +1,8 @@
+function write(){
+    var canvas = document.getElementById("canvas-chichi");
+    var ctx = canvas.getContext("2d");
+
+    ctx.font= "130px Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif";
+    ctx.fillStyle = "white"
+    ctx.fillText("CHICHI", 45,130);
+}
